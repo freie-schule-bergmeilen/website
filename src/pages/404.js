d641c82fd3ef74f12808e69f6139b1dbe955c0ec
Ilya Boyandin <ilyabo@gmail.com>
2018-05-20 23:06:37 +0200

    Improved 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <section className="section">
+    <div className="container">
+      <h2 className="title">Oh nein…</h2>
+      <p>
+        Diese Seite haben wir leider nicht gefunden.
+        Vielleicht wurde sie inzwischen gelöscht oder umbenannt.
+      </p>
+      <br/>
+      <p>
+        Schauen Sie doch <Link to="/">unsere Homepage</Link> an.
+      </p>
+    </div>
+  </section>
 )
 
 export default NotFoundPage
