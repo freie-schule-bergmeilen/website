ee06b14f08c2e02849bcaa91b0696c928cb66e75
Ilya Boyandin <ilyabo@gmail.com>
2019-05-21 17:02:36 +0200

    Disabled google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,13 +61,13 @@ module.exports = {
         ]
       }
     },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId,
-        anonymize: true,
-      },
-    },
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     trackingId,
+    //     anonymize: true,
+    //   },
+    // },
 
     {
       resolve: `gatsby-plugin-nprogress`,
